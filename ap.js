7cd3ed13c73d35c8bff7051b37187141ba022b0b
Mfayvor <mfavour918@gmail.com>
2025-03-14 05:01:50 -0500

    finished animations

--- ap.js
+++ ap.js
@@ -1,8 +1,9 @@
-
+// toggle menu/navbar script 
 $('.hamburger').click(function(x){
   $('#nav-menu').toggleClass("active");
   $('.hamburger-two').toggleClass("active")
 });
+
 
 $(document).ready(function() {
   $(window).scroll(function(){
@@ -11,9 +12,37 @@ $(document).ready(function() {
     }else{
       $('.header').removeClass("sticky");
     }
-  })
+    if(this.scrollY > 500){
+      $('.scroll-up-btn').addClass("show");
+    }else{
+      $('.scroll-up-btn').removeClass("show");
+    }
+  });
+
+  // slide up script 
+  $('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
+  });
+
+  // typing animation script 
+  var typed = new Typed(".typing",{
+    strings: ["Designer", "Developer", "Blogger", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
+  var typed = new Typed(".typing-2",{
+    strings: ["Designer", "Developer", "Blogger", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
 });
 
+
+// owl carousel script 
 $('.carousel').owlCarousel({
   margin: 20,
   loop: true,
